5cf37710821e8e09585226bcf7ada3e5380e918b
Heather <heather@imimr.net>
2019-08-18 22:56:21 +0800

    Added card list and card components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
+
     this.state = {
       monsters: []
     }
@@ -18,9 +20,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <CardList monsters={this.state.monsters}/>
       </div>
     )
   }
